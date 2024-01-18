abe7b78172bf6de10e279e214edbee6a7dd136d2
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2024-01-18 11:11:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, precision_recall_fscore_support, accuracy_score
 from sklearn.preprocessing import LabelEncoder
 import base64
@@ -13,20 +12,27 @@ def perform_prediction(df, true_labels, model):
     for col in features.select_dtypes(include='object').columns:
         features[col] = le.fit_transform(features[col])
     y_pred = model.predict(features)
+    
+    # Calculate metrics
     report = classification_report(true_labels, y_pred)
     confusion = confusion_matrix(true_labels, y_pred)
     
+    # Extract precision, recall, f1-score, and specificity from the classification report
     precision, recall, f1_score, _ = precision_recall_fscore_support(true_labels, y_pred, average='binary')
     accuracy = accuracy_score(true_labels, y_pred)
     specificity = confusion[0, 0] / (confusion[0, 0] + confusion[0, 1])
     
-    return precision, recall, f1_score, accuracy, specificity, confusion
-
+    # Create a DataFrame with actual and predicted values
+    result_df = pd.DataFrame({
+        'Actual': true_labels,
+        'Predicted': y_pred
+    })
+    
+    return precision, recall, f1_score, accuracy, specificity, confusion, result_df
 
 st.set_page_config(page_title='CSV File Uploader', page_icon=':open_file_folder:')
 st.header('Prediction Metrics for Resubmit/Returned Profiles')
 csv_file = st.file_uploader('Choose a CSV file', type='csv')
-
 
 if csv_file is not None:
     df = pd.read_csv(csv_file)
@@ -36,7 +42,7 @@ if csv_file is not None:
     else:
         model = joblib.load(open('decision_tree.pkl', 'rb'))
         true_labels = df['Resubmit_binary']
-        precision, recall, f1_score, accuracy, specificity, confusion = perform_prediction(df, true_labels, model)
+        precision, recall, f1_score, accuracy, specificity, confusion, result_df = perform_prediction(df, true_labels, model)
         st.write(f"Precision: {precision:.2%}")
         st.write(f"Recall: {recall:.2%}")
         st.write(f"F1 Score: {f1_score:.2%}")
@@ -44,5 +50,7 @@ if csv_file is not None:
         st.write(f"Specificity: {specificity:.2%}")
         st.write("Confusion Matrix:")
         st.text(confusion)
+        st.write("Prediction DataFrame:")
+        st.write(result_df)
 else:
     st.warning('Please upload a CSV file')
