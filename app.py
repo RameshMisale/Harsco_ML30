7e9a7b9487056913e03663c74c1781dec710c4af
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2024-01-19 11:42:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,22 +13,19 @@ def perform_prediction(df, true_labels, model):
         features[col] = le.fit_transform(features[col])
     y_pred = model.predict(features)
     
-    # Calculate metrics
     report = classification_report(true_labels, y_pred)
     confusion = confusion_matrix(true_labels, y_pred)
-    
-    # Extract precision, recall, f1-score, and specificity from the classification report
     precision, recall, f1_score, _ = precision_recall_fscore_support(true_labels, y_pred, average='binary')
     accuracy = accuracy_score(true_labels, y_pred)
     specificity = confusion[0, 0] / (confusion[0, 0] + confusion[0, 1])
     
-    # Create a DataFrame with actual and predicted values
     result_df = pd.DataFrame({
+        'profile_id': df['profile_id'],
         'Actual': true_labels,
         'Predicted': y_pred
     })
     
-    return precision, recall, f1_score, accuracy, specificity, confusion, result_df
+    return precision, recall, f1_score, accuracy, specificity, confusion, result_df, report
 
 st.set_page_config(page_title='CSV File Uploader', page_icon=':open_file_folder:')
 st.header('Prediction Metrics for Resubmit/Returned Profiles')
@@ -42,15 +39,21 @@ if csv_file is not None:
     else:
         model = joblib.load(open('decision_tree.pkl', 'rb'))
         true_labels = df['Resubmit_binary']
-        precision, recall, f1_score, accuracy, specificity, confusion, result_df = perform_prediction(df, true_labels, model)
+        precision, recall, f1_score, accuracy, specificity, confusion, result_df, classification_report_text = perform_prediction(df, true_labels, model)
         st.write(f"Precision: {precision:.2%}")
         st.write(f"Recall: {recall:.2%}")
         st.write(f"F1 Score: {f1_score:.2%}")
         st.write(f"Accuracy: {accuracy:.2%}")
         st.write(f"Specificity: {specificity:.2%}")
+        
         st.write("Confusion Matrix:")
         st.text(confusion)
+        st.write("Note: 1 corresponds to 'Resubmit', and 0 corresponds to 'Not Resubmit'")
+        
         st.write("Prediction DataFrame:")
         st.write(result_df)
+
+        st.write("Classification Report:")
+        st.text(classification_report_text)
 else:
     st.warning('Please upload a CSV file')
