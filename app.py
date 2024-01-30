df141fbb4675c8f4e0913685b07559c8af8b5bd0
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2024-01-30 16:11:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import classification_report, confusion_matrix, precision_recall_fscore_support, accuracy_score
 from sklearn.preprocessing import LabelEncoder
 import base64
 import joblib
@@ -11,24 +10,24 @@ def perform_prediction(df, true_labels, model):
     le = LabelEncoder()
     for col in features.select_dtypes(include='object').columns:
         features[col] = le.fit_transform(features[col])
-    y_pred = model.predict(features)
     
-    report = classification_report(true_labels, y_pred)
-    confusion = confusion_matrix(true_labels, y_pred)
-    precision, recall, f1_score, _ = precision_recall_fscore_support(true_labels, y_pred, average='binary')
-    accuracy = accuracy_score(true_labels, y_pred)
-    specificity = confusion[0, 0] / (confusion[0, 0] + confusion[0, 1])
+    # Get probability values for both classes
+    probability_values = model.predict_proba(features)
+    
+    predicted_labels = model.predict(features)
     
     result_df = pd.DataFrame({
         'profile_id': df['profile_id'],
         'Actual': true_labels,
-        'Predicted': y_pred
+        'Predicted': predicted_labels,
+        'Probability_Class_0': probability_values[:, 0],
+        'Probability_Class_1': probability_values[:, 1]
     })
     
-    return precision, recall, f1_score, accuracy, specificity, confusion, result_df, report
+    return result_df
 
 st.set_page_config(page_title='CSV File Uploader', page_icon=':open_file_folder:')
-st.header('Predicting the Potential Profiles that might go into  Resubmit/Returned stage')
+st.header('Predicting the Potential Profiles that might go into Resubmit/Returned stage')
 csv_file = st.file_uploader('Choose a CSV file', type='csv')
 
 if csv_file is not None:
@@ -46,39 +45,17 @@ if csv_file is not None:
     else:
         model = joblib.load(open('decision_tree.pkl', 'rb'))
         true_labels = df['Resubmit_binary']
-        precision, recall, f1_score, accuracy, specificity, confusion, result_df, classification_report_text = perform_prediction(df, true_labels, model)
+        result_df = perform_prediction(df, true_labels, model)
+
+        st.write(" ")
+        st.write(" ")
 
         # Display Result DataFrame on top
-        st.write("  ")
-        st.write("  ")
         st.write("1 corresponds to 'Resubmit', and 0 corresponds to 'Not Resubmit'")
-        st.write("  ")
-        st.write("Output Results")
+        st.write(" ")
+        st.write(" ")
         st.write("Result DataFrame:")
-        st.write("  ")
         st.write(result_df)
-        st.write("  ")
-        st.write("  ")
 
-        # Display Metrics Below Result DataFrame
-        st.write("Confusion Matrix:")
-        st.text(confusion)
-        st.write("  ")
-        st.write("  ")
-        st.write("Precision: {:.2%}".format(precision))
-        st.write("Recall: {:.2%}".format(recall))
-        st.write("F1 Score: {:.2%}".format(f1_score))
-        st.write("Accuracy: {:.2%}".format(accuracy))
-        st.write("Specificity: {:.2%}".format(specificity))
-        st.write("  ")
-        st.write("  ")
-        st.write("  ")
-        
-        # st.write("Confusion Matrix:")
-        # st.text(confusion)
-        # st.write("Note: 1 corresponds to 'Resubmit', and 0 corresponds to 'Not Resubmit'")
-        
-        st.write("Classification Report:")
-        st.text(classification_report_text)
 else:
     st.warning('Please upload a CSV file')
