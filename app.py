964d78ff1b199a70225b1e385b0685d71049b61b
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2024-02-06 13:25:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,54 +4,59 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder
 import joblib
 from PIL import Image
- 
+
 def perform_prediction(features, model):
     le = LabelEncoder()
     for col in features.select_dtypes(include='object').columns:
         features[col] = le.fit_transform(features[col])
+    
     probability_values = model.predict_proba(features)
+    
     predicted_labels = model.predict(features)
+    
     result_df = pd.DataFrame({
         'Predicted': predicted_labels,
         'Probability_Class_0': probability_values[:, 0],
         'Probability_Class_1': probability_values[:, 1]
     })
+    
     return result_df
- 
+
 # Set page configuration and add logo
 st.set_page_config(page_title='Manual Entry Predictor', page_icon=':clipboard:', layout='wide', initial_sidebar_state='expanded')
 logo = Image.open('Logo.jpg')  
 st.image(logo, use_column_width=False, width=200)
- 
+
 # Set background color using CSS
 st.markdown(
     """
-<style>
+    <style>
         body {
             background-color: #78BE20 !important;  /* Light green color */
         }
-        .stTextInput > div > div > input {
-            background-color: #78BE20 !important;  /* Light green color */
-            color: white !important;
+        .stTextInput input[type="number"]::-webkit-inner-spin-button,
+        .stTextInput input[type="number"]::-webkit-outer-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
         }
-</style>
+    </style>
     """,
     unsafe_allow_html=True
 )
- 
+
 # Header and manual entry of features
 st.markdown(
     """
-<div style="background-color: #78BE20; padding: 10px; border-radius: 10px;">
-<h1 style="color: white;">PROFILE RESUBMIT PREDICTION</h1>
-</div>
+    <div style="background-color: #78BE20; padding: 10px; border-radius: 10px;">
+        <h1 style="color: white;">PROFILE RESUBMIT PREDICTION</h1>
+    </div>
     """,
     unsafe_allow_html=True
 )
- 
+
 # Create 3 columns for better layout
 col1, col2, col3 = st.columns(3)
- 
+
 # Input features (distribute across the columns)
 features = {}
 for idx, feature in enumerate(['profile_id', 'flash_point_flag', 'water_reactivity_flag', 'monolith_flag', 'sulfides_reactivity_flag',
@@ -62,10 +67,10 @@ for idx, feature in enumerate(['profile_id', 'flash_point_flag', 'water_reactivi
                               'sludges_flag', 'pcbs_flag', 'halogens_flag', 'cyanides_flag', 'pesticides_flag', 'dot_explosive_flag',
                               'boiling_point_flag', 'isRecertified', 'labpack_flag', 'national_flag', 'urgent_flag', 'rush_flag']):
     with col1 if idx % 3 == 0 else col2 if idx % 3 == 1 else col3:  # Place features in alternating columns
-        features[feature] = st.number_input(feature, value=0, step=1)
- 
+        features[feature] = st.text_input(feature, value="0")
+
 profile_id = st.empty()
- 
+
 if st.button('Predict'):
     profile_id.text(f"Profile ID: {features['profile_id']}")  # Display profile ID
     data = {feature: [value] for feature, value in features.items()}
@@ -79,6 +84,6 @@ if st.button('Predict'):
         st.success(f"The profile is going into Resubmit stage with a confidence of {confidence_class_1:.2%}.")
     else:
         st.success(f"The profile is not going into Resubmit stage with a confidence of {confidence_class_0:.2%}.")
- 
+
 st.markdown("____________________________________________________________________________________")
 st.markdown("2024 Clean Earth")
